e8ea469b89a3221cc16971771c806e1dc3f6f7b9
Somayeh <mail.somayeh@gmail.com>
2018-06-21 12:44:41 +0100

    Add data to test database before running feature test

--- spec/features/viewing_peeps_spec.rb
+++ spec/features/viewing_peeps_spec.rb
@@ -1,5 +1,10 @@
+require 'pg'
 feature 'Viewing peeps' do
   scenario 'A user can see peeps' do
+    connection = PG.connect(dbname: 'chitter1_test')
+    # Add the test data
+    connection.exec("INSERT INTO peeps VALUES(1, 'capybara feature test');")
+    connection.exec("INSERT INTO peeps VALUES(2, 'setting up database');")
     visit('/')
     expect(page).to have_content 'capybara feature test'
     expect(page).to have_content 'setting up database'
