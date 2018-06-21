2f8beffeb077a550eb3e6c51a14253658ae3151b
Somayeh <mail.somayeh@gmail.com>
2018-06-21 13:57:26 +0100

    Add data to test database before running unit test

--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -1,7 +1,12 @@
 require 'peep'
+require 'pg'
  describe Peep do
    describe '.all' do
      it 'return all peeps' do
+       connection = PG.connect(dbname: 'chitter1_test')
+       # Add the test data
+       connection.exec("INSERT INTO peeps (message) VALUES ('capybara feature test');")
+       connection.exec("INSERT INTO peeps (message) VALUES ('setting up database');")
        peeps = Peep.all
        expect(peeps).to include 'capybara feature test'
        expect(peeps).to include 'setting up database'
