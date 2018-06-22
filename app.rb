549d3042413838a6ac814f6208b9a2313a5cdcbf
Somayeh <mail.somayeh@gmail.com>
2018-06-22 17:51:18 +0100

    Delete extra lines from code which were added for more visibility

--- app.rb
+++ app.rb
@@ -12,16 +12,13 @@ class Chitter < Sinatra::Base
   end
 
   post '/' do
-    p params
     mes = params['message']
-    p mes
     if ENV['ENVIRONMENT'] == 'test'
       connection = PG.connect(dbname: 'chitter1_test')
     else
       connection = PG.connect(dbname: 'chitter1')
     end
-    p connection
-    p connection.exec("INSERT INTO peeps (message) VALUES('#{mes}')")
+    connection.exec("INSERT INTO peeps (message) VALUES('#{mes}')")
     redirect '/'
   end
 
