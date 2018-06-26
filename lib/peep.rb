cf5211983f1ab86ef53c6a449943440793cec8b3
Somayeh <mail.somayeh@gmail.com>
2018-06-26 11:32:46 +0100

    Add Create method

--- lib/peep.rb
+++ lib/peep.rb
@@ -10,4 +10,13 @@ class Peep
     result.map { |peep| peep['message'] }
   end
 
+  def self.create(options)
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'chitter1_test')
+    else
+      connection = PG.connect(dbname: 'chitter1')
+    end
+
+    connection.exec("INSERT INTO peeps (message) VALUES('#{options[:mes]}')")
+  end
 end
