7e83f8a84a19e3fbf654c44c48c3ebc1d689f11f
Somayeh <mail.somayeh@gmail.com>
2018-06-26 11:46:54 +0100

    Refactor duumy data insertion to database for test

--- spec/features/viewing_peeps_spec.rb
+++ spec/features/viewing_peeps_spec.rb
@@ -1,10 +1,8 @@
 require 'pg'
 feature 'Viewing peeps' do
   scenario 'A user can see peeps' do
-    connection = PG.connect(dbname: 'chitter1_test')
-    # Add the test data
-    connection.exec("INSERT INTO peeps VALUES(1, 'capybara feature test');")
-    connection.exec("INSERT INTO peeps VALUES(2, 'setting up database');")
+    Peep.create(mes: 'capybara feature test')
+    Peep.create(mes: 'setting up database')
     visit('/')
     expect(page).to have_content 'capybara feature test'
     expect(page).to have_content 'setting up database'
