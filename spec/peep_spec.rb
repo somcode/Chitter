bdef05d94e281e071d7e00824690451b6390dbef
Somayeh <mail.somayeh@gmail.com>
2018-06-26 11:26:47 +0100

    Test to add a new create method

--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -1,15 +1,22 @@
 require 'peep'
 require 'pg'
- describe Peep do
-   describe '.all' do
-     it 'return all peeps' do
-       connection = PG.connect(dbname: 'chitter1_test')
-       # Add the test data
-       connection.exec("INSERT INTO peeps (message) VALUES ('capybara feature test');")
-       connection.exec("INSERT INTO peeps (message) VALUES ('setting up database');")
-       peeps = Peep.all
-       expect(peeps).to include 'capybara feature test'
-       expect(peeps).to include 'setting up database'
-     end
-   end
- end
+describe Peep do
+  describe '.all' do
+    it 'return all peeps' do
+      connection = PG.connect(dbname: 'chitter1_test')
+      # Add the test data
+      connection.exec("INSERT INTO peeps (message) VALUES ('capybara feature test');")
+      connection.exec("INSERT INTO peeps (message) VALUES ('setting up database');")
+      peeps = Peep.all
+      expect(peeps).to include 'capybara feature test'
+      expect(peeps).to include 'setting up database'
+    end
+  end
+
+  describe '.create' do
+    it 'creates a new bookmark' do
+      Peep.create(mes: 'Hello World!')
+      expect(Peep.all).to include 'Hello World!'
+    end
+  end
+end
